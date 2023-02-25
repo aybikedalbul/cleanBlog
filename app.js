4134cd2046c5722cd870d29d7c37d62d85f4bfd5
Aybike <100150878+aybikedalbul@users.noreply.github.com>
2023-02-25 11:46:09 +0300

    Second commit

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 const express = require("express");
-
+const ejs = require("ejs");
+const path = require("path");
 const app = express();
 
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+// MIDDLEWARE
+const myLogger = (req, res, next) => {
+  console.log("Middleware Log 1 is running..");
+  next();
+};
+
+app.use(myLogger);
 app.get("/", (req, res) => {
-  const blog = { id: 1, title: "Blog title", description: "Blog description" };
-  res.send(blog);
+  res.render("index");
+});
+// Routes: 
+app.get("/index", (req, res) => {
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/add", (req, res) => {
+  res.render("add");
 });
 
 const port = 3002;
