07df0b7d1754f56d757dc39ee935de397711c182
Aybike <100150878+aybikedalbul@users.noreply.github.com>
2023-02-28 15:40:54 +0300

    Third Commit

--- app.js
+++ app.js
@@ -1,34 +1,46 @@
 const express = require("express");
 const ejs = require("ejs");
 const path = require("path");
-const app = express();
+const mongoose = require("mongoose");
+const Photo = require('./models/Photo');
 
+const app = express();
+ 
+mongoose.connect("mongodb://127.0.0.1:27017/cleanblog-test-db");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-// MIDDLEWARE
-const myLogger = (req, res, next) => {
-  console.log("Middleware Log 1 is running..");
-  next();
-};
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(myLogger);
-app.get("/", (req, res) => {
-  res.render("index");
-});
-// Routes: 
-app.get("/index", (req, res) => {
-  res.render("index");
+
+
+// Routes:
+app.get("/", async (req, res) => {
+  const photos = await Photo.find({});
+  res.render('index', {
+    photos,
+  });
 });
 
 app.get("/about", (req, res) => {
   res.render("about");
 });
+app.get("/index", (req, res) => {
+  res.render("index");
+});
 
 app.get("/add", (req, res) => {
   res.render("add");
 });
+app.get("/post", (req, res) => {
+  res.render("post");
+});
 
+app.post('/photos', async  (req, res) => {
+  await Photo.create(req.body);
+  res.redirect("/");
+});
 const port = 3002;
 
 app.listen(port, () => {
